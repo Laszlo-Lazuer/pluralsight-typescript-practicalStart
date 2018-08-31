daea3a8c2e2e3b064acefa6fad8414bc2e73a6f6
Laszlo-Lazuer <visualjazz@gmail.com>
2018-08-31 08:57:18 -0500

    Adds knockout.

--- vendingmachine/js/app.js
+++ vendingmachine/js/app.js
@@ -33,7 +33,6 @@ var VendingMachine = /** @class */ (function () {
     return VendingMachine;
 }());
 /// <reference path="vendingMachine.ts" />
-/// <reference path="typings/knockout.d.ts" />
 var machine = new VendingMachine();
 ko.applyBindings(machine);
 //# sourceMappingURL=app.js.map
